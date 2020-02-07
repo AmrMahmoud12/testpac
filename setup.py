e9afadf48f628920bedc4a5f1a1e7e80c567d453
amr <ammah246@gmail.com>
2020-02-07 23:35:56 +0200

    hi first

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="amrpackagetest", # Replace with your own username
+    name="AmrMahmoud12", # Replace with your own username
     version="0.0.1",
     author="amr mahmoud",
     author_email="ammah246@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/amrpackagetest",
+    url="https://github.com/AmrMahmoud12/testpac",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
